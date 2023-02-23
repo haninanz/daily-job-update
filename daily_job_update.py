d8cee2c92a063cb5a63578c6bbbc8e3564294752
haninanz <haninanz@gmail.com>
2023-02-23 14:36:12 +0700

    Updated all html and css and added config.py

--- daily_job_update.py
+++ daily_job_update.py
@@ -1,17 +1,55 @@
-from flask import Flask, render_template
+import flask
+from flask import render_template, request, redirect, url_for
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
+app.config.from_pyfile('config.py')
+
+job_updates = {'external': [], 'internal': []}
 
 @app.route('/')
 def home():
-	return render_template('job_update.html')
+	return render_template('job_update.html', job_updates=job_updates)
 
-@app.route('/internal/')
+@app.route('/internal/', methods=['GET', 'POST'])
 def internal_job_input():
+	if request.method == 'POST':
+		date = request.form['today_date']
+		name = request.form['name']
+		status = request.form['status']
+		if not request.form['project_name']:
+			project_name = None
+		else:
+			project_name = request.form['project_name']
+
+		job_updates['internal'].append({
+			'date': date,
+			'project_name': project_name,
+			'name': name,
+			'status': status
+			})
+		return redirect(url_for('home'))
 	return render_template('input_internal.html')
 
-@app.route('/external/')
+@app.route('/external/', methods=['GET', 'POST'])
 def external_job_input():
+	if request.method == 'POST':
+		date = request.form['today_date']
+		project_name = request.form['project_name']
+		members = request.form['member_name']
+		ride = request.form['ride_no']
+		funds = request.form['funds']
+		time = request.form['time']
+
+		job_updates['external'].append({
+			'date': date,
+			'project_name': project_name,
+			'members': members,
+			'ride': ride,
+			'funds': funds,
+			'time': time
+			})
+		return redirect(url_for('home'))
+		
 	return render_template('input_external.html')
 
 if __name__ == '__main__':
