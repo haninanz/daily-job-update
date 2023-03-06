cbf33a2f28ccdda784b1ab8bf18ab63bf05a88ac
haninanz <haninanz@gmail.com>
2023-03-06 11:10:12 +0700

    Modified web updates

--- daily_job_update.py
+++ daily_job_update.py
@@ -1,4 +1,3 @@
-import os
 import json
 import flask
 from flask import render_template, request, redirect, url_for, json
@@ -6,38 +5,56 @@ from flask import render_template, request, redirect, url_for, json
 app = flask.Flask(__name__)
 app.config.from_pyfile('config.py')
 
-job_updates = {}
+working_date = []
+external_jobs = {}
+internal_jobs = {}
 
 @app.route('/')
 def home():
-	global job_updates
-	if not job_updates:
-		with open('data/placeholder_data.json', 'r') as json_file:
-			job_updates = json.load(json_file)
-	return render_template('job_update.html', job_updates=job_updates)
+	global external_jobs, internal_jobs
+	if not external_jobs:
+		with open('data/placeholder_external.json', 'r') as external_json:
+			external_jobs = json.load(external_json)
+	if not internal_jobs:
+		with open('data/placeholder_internal.json', 'r') as internal_json:
+			internal_jobs = json.load(internal_json)
+	# if external_jobs and internal_jobs:
+	# 	job_updates = {'external': external_jobs, 'internal': internal_jobs}
+	# 	with open('data/job_updates.json', 'w') as jobs_json:
+	# 		json.dump(job_updates, jobs_json)
+
+	working_date = list(set(list(external_jobs.keys()) + list(internal_jobs.keys())))
+
+	return render_template('job_update.html',
+		working_date=working_date,
+		external_jobs=external_jobs,
+		internal_jobs=internal_jobs
+		)
 
 @app.route('/internal/', methods=['GET', 'POST'])
 def internal_job_input():
+	global internal_jobs
 	if request.method == 'POST':
 		date = request.form['today_date']
 		name = request.form['name']
 		status = request.form['status']
 		project_name = request.form['project_name']
 
-		if date not in job_updates:
-			job_updates[date] = []
-		job_updates[date].append({
+		if date not in internal_jobs:
+			internal_jobs[date] = []
+		internal_jobs[date].append({
 			'type': 'internal',
 			'name': name,
 			'status': status,
 			'project_name': project_name
 			})
-
 		return redirect(url_for('home'))
+
 	return render_template('input_internal.html')
 
 @app.route('/external/', methods=['GET', 'POST'])
 def external_job_input():
+	global external_jobs
 	if request.method == 'POST':
 		date = request.form['today_date']
 		project_name = request.form['project_name']
@@ -47,9 +64,9 @@ def external_job_input():
 		funds = request.form['funds']
 		time = request.form['time']
 
-		if date not in job_updates:
-			external_job_updates[date] = []
-		job_updates[date].append({
+		if date not in external_jobs:
+			external_jobs[date] = []
+		external_jobs[date].append({
 			'type': 'external',
 			'project_name': project_name,
 			'members': members,
@@ -58,7 +75,6 @@ def external_job_input():
 			'funds': funds,
 			'time': time
 			})
-
 		return redirect(url_for('home'))
 		
 	return render_template('input_external.html')
